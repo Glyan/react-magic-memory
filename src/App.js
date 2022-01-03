f16fa309f794035b80764dce7734396ad165bcb8
Glyan <pamela27jr@gmail.com>
2022-01-03 16:40:13 +0000

    Matching cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useState, useEffect } from 'react'
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" }
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false }
 ];
 
 function App() {
@@ -33,12 +33,18 @@ function App() {
 
   useEffect (() => {
     if (choiceOne && choiceTwo) {
-      if (choiceOne.src === choiceTwo.src)
-        console.log('Match!')
-      else
-        console.log('Wrong :(')
-
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src)
+              return {...card, matched: true}
+            else 
+              return card
+          })
+        })        
+      }
       resetTurn();
+      console.log(cards);
     }
   }, [choiceOne, choiceTwo])
 
